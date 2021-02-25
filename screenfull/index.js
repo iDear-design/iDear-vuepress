d55f81cc88fdbe0f5493200b997d5f1d933a4d06
maxiaoqu <maxiaoqu@163.com>
2021-02-25 11:09:08 +0800

    screenFull

--- screenfull/index.js
+++
@@ -1,8 +0,0 @@
-const { path } = require('@vuepress/shared-utils')
-
-module.exports = (options, context) => ({
-  name: '@idear-vuepress/screenfull',
-  enhanceAppFiles: [
-    path.resolve(__dirname, './bin/enhanceAppFile.js')
-  ]
-})
