6033722fbc84f50cccd1d4dac8bf0fa471d79974
maxiaoqu <maxiaoqu@163.com>
2021-02-27 10:07:52 +0800

    extractCode

---
+++ vuepress-plugin-extractCode/index.js
@@ -0,0 +1,13 @@
+const { path } = require('@vuepress/shared-utils')
+const extractCodeFromVueSFC = require('./bin/extractCodeFromVueSFC.js')
+
+module.exports = (options, context) => ({
+  name: '@idear-vuepress/vuepress-plugin-extractCode',
+  chainMarkdown (config) {
+    config.plugin('extractCode')
+      .use(extractCodeFromVueSFC)
+  },
+  enhanceAppFiles: [
+    path.resolve(__dirname, './bin/enhanceAppFile.js')
+  ]
+})
